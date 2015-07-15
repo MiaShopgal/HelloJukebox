dd85c9d40ac40fd04b95d63b4617ac87ad2ce887
Mia <mia.shopgal@gmail.com>
2015-07-15 15:08:35 +0800

    add macro for swizzling check

---
+++ HelloJukeBox/JukeBoxMacro.h
@@ -0,0 +1,10 @@
+//
+// Created by Mia Yu on 7/15/15.
+// Copyright (c) 2015 Miao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface JukeBoxMacro : NSObject
+@end
---
+++ HelloJukeBox/JukeBoxMacro.m
@@ -0,0 +1,12 @@
+//
+// Created by Mia Yu on 7/15/15.
+// Copyright (c) 2015 Miao. All rights reserved.
+//
+
+#import "JukeBoxMacro.h"
+
+
+@implementation JukeBoxMacro {
+
+}
+@end
